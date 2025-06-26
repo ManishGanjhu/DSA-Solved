f9709cbcff5523c90d9e039cde6df6c886b7915b
Manish Ganjhu <manishkumarganjhu.01@gmail.com>
2025-06-26 22:03:18 +0530

    code to find largest in array

---
+++ Arrays-1/largestInArray.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+
+public class largestInArray {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,40,8,7,9,10,45,67};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-1]);
+        
+    }
+}
