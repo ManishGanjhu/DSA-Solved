ad53fb6f8125f6ac09852c341e6d9db4802760d3
Manish Ganjhu <manishkumarganjhu.01@gmail.com>
2025-07-01 18:15:21 +0530

    Added code for checking odd or even

---
+++ Bit-Manipulation/OddOrEven.java
@@ -0,0 +1,12 @@
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int a = 121;
+        if((a&1)==1){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+    }
+
+}
