fe2941a31560a511cb63c3af47e6ce290948d7cd
Manish Ganjhu <manishkumarganjhu.01@gmail.com>
2025-07-01 07:51:07 +0530

    Code for Insertion sort

---
+++ Basic-Sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[]={5,3,1,2,4};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void insertion(int arr[]){
+        int n = arr.length;
+         // we have to start from second element cause start is already sorted
+         for(int i = 1 ; i < n;i++){
+            int curr = arr[i]; // element to insert
+            int j = i-1;
+
+            while(j>=0 && arr[j]>curr){
+                arr[j+1]=arr[j]; // shift element right
+                j--;// move left
+            }
+            // insert current at the right position
+            arr[j+1]=curr;
+
+            // check the track
+            // System.out.println(Arrays.toString(arr));
+         }
+    }
+}
