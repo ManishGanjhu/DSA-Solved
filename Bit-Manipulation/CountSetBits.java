fb63cb5c3db60073f96e53c35e865f697816ca1a
Manish Ganjhu <manishkumarganjhu.01@gmail.com>
2025-07-06 12:22:15 +0530

    Added code to count set bits

---
+++ Bit-Manipulation/CountSetBits.java
@@ -0,0 +1,7 @@
+public class CountSetBits {
+    public static void main(String[] args) {
+        int n = 45;
+        int count = Integer.bitCount(n);
+        System.out.println(count);
+    }
+}
